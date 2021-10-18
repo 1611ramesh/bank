4a350288c581475cea20792f822a9bfaaff1830b
1611ramesh <1611ramesh1995@gmail.com>
2021-10-18 19:43:13 +0530

    added bank details

--- src/org/bank/Bank.java
+++ src/org/bank/Bank.java
@@ -5,6 +5,11 @@ public class Bank {
 		String s = "green tech";
 		System.out.println("change to new branch");
 		System.out.println("chenage to new bank");
+		System.out.println("change to new branch");
+		System.out.println("change to new branch");
+		System.out.println("change to new branch");
+		System.out.println("change to new branch");
+		System.out.println("change to new branch");
 		System.out.println("get bank details");
 		System.out.println("get pan no");
 		System.out.println("get bank details");
